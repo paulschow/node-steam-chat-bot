0d24a426081229e896a63dffd3b28ce0a6825c72
Paul Schow <paul.schow@gmail.com>
2014-10-19 19:20:43 -0600

    Adding some comments

--- movelogs.py
+++ movelogs.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# This is a little python script that takes the logs, 
+# renames them with the date, 
+# and moves them to a logs folder.
+
+# Use this with a cron that looks something like this
+
+# 59 23 * * * /usr/bin/python /home/paul/node_modules/steam-chat-bot/movelogs.py
+# 58 23 * * * killall nodejs
+# 1 0 * * * cd /home/paul/node_modules/steam-chat-bot/ && /usr/bin/nodejs /home/paul/node_modules/steam-chat-bot/autopaul.js
+
+# Then at midnight the bot will shut down, move logs, and restart
+# There's probably a better way to do this
+
+
 #import os
 import shutil
 import time
@@ -9,8 +23,8 @@ date = time.strftime("%d%b%Y")
 #print date
 
 try:
-    shutil.move('/home/paul/node_modules/steam-chat-bot/bot.autopaul.log',
-         '/home/paul/node_modules/steam-chat-bot/logs/%s.txt' % date)
+    shutil.move('/home/paul/node_modules/node-steam-chat-bot/bot.autopaul.log',
+         '/home/paul/node_modules/node-steam-chat-bot/logs/%s.txt' % date)
 except IOError:
     print "Error: No Logs Found"
 
